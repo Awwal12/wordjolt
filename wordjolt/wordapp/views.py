77eaab7c079e9d01941841484629d7d78319a3af
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-01 17:41:53 +0100

    first home page setup

--- wordjolt/wordapp/views.py
+++ wordjolt/wordapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+
+def home(request):
+    response = requests.get(
+        'https://juanroldan1989-moviequotes-v1.p.rapidapi.com/api/v1/quotes').json()
+    return render(request, 'html/home.html', {'response': response})
