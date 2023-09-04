cda31645088a097e8cdac2ab6d174e9a0e95131e
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-04 12:40:54 +0100

    added update

--- wordjolt/wordapp/views.py
+++ wordjolt/wordapp/views.py
@@ -4,9 +4,8 @@ import requests
 
 
 def home(request):
-    url = "https://api.quotable.io/quotes/random"
+    # url = "https://api.quotable.io/quotes/random"
 
-    response = requests.get(url).json()
-    quote_content = response.get('content', '')
-
-    return render(request, 'html/home.html', {'quote_content': quote_content})
+    # response = requests.get(url).json()
+# , {'response':response }
+    return render(request, 'html/home.html')
