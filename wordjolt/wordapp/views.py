adf38fff0da569320433e09a806a0d4faf46d53e
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-06 11:56:02 +0100

    functionality done

--- wordjolt/wordapp/views.py
+++ wordjolt/wordapp/views.py
@@ -4,11 +4,18 @@ import requests
 
 
 def home(request):
-    # url = "https://api.quotable.io/quotes/random"
-
-    # response = requests.get(url).json()
-# , {'response':response }
     return render(request, 'html/home.html')
 
+
 def main(request):
-    return render(request, 'html/main.html')
+    url = "https://api.quotable.io/quotes/random"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+            api_data = response.json()
+    else:
+        api_data = None
+
+    context = {'api_data':api_data}
+    return render(request, 'html/main.html', context)
+
