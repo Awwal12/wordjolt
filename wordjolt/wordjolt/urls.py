e985181f83804b4960ff269c97bfa98ff1d2dfb9
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-08 21:32:41 +0100

    update to the paths

--- wordjolt/wordjolt/urls.py
+++ wordjolt/wordjolt/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('wordapp.urls')),
+    path('', include('wordapp.urls')),
 ]
